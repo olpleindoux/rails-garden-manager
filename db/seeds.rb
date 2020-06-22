27cf0465e96e3b17ab527ef3b553a80f9285a04c
Olivier Pleindoux <pleindoux.olivier@gmail.com>
2020-06-22 14:21:36 +0200

    add some informations for user during seeding database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+puts "Cleaning the database"
 Garden.destroy_all
 
 garden_names = [
@@ -10,12 +11,14 @@ garden_names = [
 ]
 
 garden_names.each do |garden_name|
+  puts "Creation of #{garden_name}"
   garden_request = RestClient.get("https://source.unsplash.com/1200x700/?garden")
   garden = Garden.new(
       name: garden_name,
       banner_url: garden_request.request.url
     )
   garden.save!
+  puts "and its 3 plants "
   3.times do
     plant_request = RestClient.get("https://source.unsplash.com/400x300/?flower")
     plant = Plant.new(
@@ -27,3 +30,5 @@ garden_names.each do |garden_name|
     sleep(2)
   end
 end
+
+puts "Finished! #{Garden.count} gardens and #{Plant.count} plants have been created"
